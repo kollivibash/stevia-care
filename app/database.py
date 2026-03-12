35c406e7f7805637c72486c62c529a63722aeb30
Kolli Vibash <kollivibhash@gmail.com>
2026-03-12 09:15:19 +0530

    Fix MongoDB connection

--- app/database.py
+++ app/database.py
@@ -1,19 +1,33 @@
+
 from motor.motor_asyncio import AsyncIOMotorClient
-from app.config import settings
+
+import os
 
 client = None
+
 db = None
 
 async def connect_db():
+
     global client, db
-    client = AsyncIOMotorClient(settings.mongodb_url)
-    db = client.healthcopilot
-    print("Connected to MongoDB")
+
+    mongodb_url = os.environ.get("MONGODB_URL", "mongodb+srv://healthpilot:Health2024@cluster0.y7no71s.mongodb.net/healthpilot?appName=Cluster0")
+
+    client = AsyncIOMotorClient(mongodb_url)
+
+    db = client.healthpilot
+
+    print(f"Connected to MongoDB successfully!")
 
 async def close_db():
+
     global client
+
     if client:
+
         client.close()
 
 def get_db():
+
     return db
+
