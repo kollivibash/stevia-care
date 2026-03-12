998c6cd461345f723b60e570f9a67b9634e4fc88
Kolli Vibash <kollivibhash@gmail.com>
2026-03-12 10:06:07 +0530

    Debug MongoDB URL

--- app/database.py
+++ app/database.py
@@ -1,33 +1,21 @@
-
 from motor.motor_asyncio import AsyncIOMotorClient
-
 import os
 
 client = None
-
 db = None
 
 async def connect_db():
-
     global client, db
-
-    mongodb_url = os.environ.get("MONGODB_URL", "mongodb+srv://healthpilot:Health2024@cluster0.y7no71s.mongodb.net/healthpilot?appName=Cluster0")
-
+    mongodb_url = os.environ.get("MONGODB_URL")
+    print(f"Using MongoDB URL: {mongodb_url[:50] if mongodb_url else 'NOT SET'}...")
     client = AsyncIOMotorClient(mongodb_url)
-
     db = client.healthpilot
-
     print(f"Connected to MongoDB successfully!")
 
 async def close_db():
-
     global client
-
     if client:
-
         client.close()
 
 def get_db():
-
     return db
-
